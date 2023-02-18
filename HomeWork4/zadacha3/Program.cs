6155c459b93b84451e43749374275c40114ae563
traplord92 <traplord345@gmail.com>
2023-02-18 14:34:23 +0500

    HomeWork4 Done

---
+++ HomeWork4/zadacha3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+
+int userdata = ReadInt("Введите длинну массива: ");
+
+int [] randomArray = new int[userdata];
+
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,9);
+    Console.Write(randomArray[i] + " ");
+}
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
